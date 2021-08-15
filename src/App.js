c5ad6314b53e40c943549c21963dd644b8ddc853
Aleksei Semiguk <alekceysem@yandex.ru>
2021-08-15 22:13:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Message } from "./components/Message";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text="Приветствую Вас в моем первом приложении ReactJS" />
+    </div>
+  );
+}
+
+export default App;
